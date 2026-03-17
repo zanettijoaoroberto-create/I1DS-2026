567f9e0fd401bc1418944b617267a0d2b33a31a6
zanettijoaoroberto <zanettijoaoroberto@gmail.com>
2026-03-17 14:06:33 -0300

    Vetor.js

--- aulas/08-vetor.js
+++ aulas/08-vetor.js
@@ -28,3 +28,48 @@ console.log("O Vetor agora tem", dinos.length, "elementos");
 console.log("1ª posição:", dinos[0]);
 console.log("4ª posição:", dinos[3]);
 console.log("20ª posição:", dinos[20]);
+
+// pop -> Remover um elemento final do vetor (Ultimo elemento)
+dinos.pop();
+console.table(dinos);
+console.log("O Vetor agora tem", dinos.length, "elementos");
+
+// shift ->  Remover um elemento do inicio do vetor (Primeiro elemento)
+dinos.shift();
+console.table(dinos);
+console.log("O Vetor agora tem", dinos.length, "elementos");
+
+// splice -> Remove um elemento a partir de um indice, e tambem precisa ser informada a quantidade de registro a ser excluido a partir do indice.
+dinos.splice(1, 1);// Remove somente o segundo item da lista
+console.table(dinos);
+console.log("O Vetor agora tem", dinos.length, "elementos");
+
+// Adicionar mais elemento ao Vetor
+dinos.push("Pterodáctilo");
+dinos.push("Estegossauro");
+dinos.push("Argentinossauro");
+
+// Procurar um elemento a partir do nome (descrição)
+// indexOf(procurado) -> retorna o indice do elemento vetor 
+let procurado = "Estegossauro"
+let indice = dinos.indexOf(procurado);
+console.log("O ", procurado, "esta no indice:", indice);
+
+// indexOf(procurado) -> retorna -1 caso nao encontre o elemento
+procurado = "Cristianossauro"
+indice = dinos.indexOf(procurado);
+console.log("O ", procurado, "esta no indice:", indice);
+
+// Excluir um elemento com base em seu nome 
+// 1º localizar o indice do elemento a partir do nome
+// 2º excluir o registro com base no indice retornado 
+
+procurado = "Estegossauro"
+indice = dinos.indexOf(procurado)
+console.log("O ", procurado, "esta no indice:", indice);
+dinos.splice(indice, 1);
+console.table(dinos);
+
+// Alterar o valor de um elemento a partir do indice
+dinos[1] = "Anquilossauro";
+console.table(dinos);
