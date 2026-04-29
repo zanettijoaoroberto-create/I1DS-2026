0adb841cf3ed2feb79bbb383ba51ba10299dc760
zanettijoaoroberto-create <zanettijoaoroberto@gmail.com>
2026-04-29 11:27:41 -0300

    tudo

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -18,23 +18,37 @@ const zerar = () => {
   contador.innerHTML = 0;
 };
 
-
-/* EXERCICIO 02 */ 
+/* EXERCICIO 02 */
 const mudarCor = () => {
   // Capturar a cor selecionada pelo usuário
   let cor = document.getElementById("colorPicker").value;
-    // Buscar o elemento-alvo (quadrado) 
+  // Buscar o elemento-alvo (quadrado)
   let quadrado = document.getElementById("quadrado");
   // Aplicar a cor ao elemento. (Alterar o css do background-color)
   quadrado.style.backgroundColor = cor;
-}
+};
 
 /* EXERCICIO 03 */
-let num1 = parseInt(document.getElementById("num1").value);
-let num2 = parseInt(document.getElementById("num2").value);
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
 let resultado = document.getElementById("resultado");
 
 const somar = () => {
-    let result = num1 + num2;
-    resultado.innerHTML = parseFloat(result);
+  let valor = parseFloat(num1.value) + parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const subtrair = () => {
+  let valor = parseFloat(num1.value) - parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const multiplicar = () => {
+  let valor = parseFloat(num1.value) * parseFloat(num2.value);
+  resultado.innerText = valor;
+};
+
+const dividir = () => {
+  let valor = parseFloat(num1.value) / parseFloat(num2.value);
+  resultado.innerText = valor;
 };
